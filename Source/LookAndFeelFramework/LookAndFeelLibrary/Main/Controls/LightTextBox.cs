acce562505fd0d3fb38438e33acb9c557b879652
pdtlrcbb <boy_angel4893@yahoo.com>
2015-06-11 12:15:53 +0700

    Update new light controls
    Update new light controls

---
+++ Source/LookAndFeelFramework/LookAndFeelLibrary/Main/Controls/LightTextBox.cs
@@ -0,0 +1,50 @@
+using LookAndFeelLibrary.Main.Components;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace LookAndFeel.Controls
+{
+    public class LightTextBox : TextBox, IControl
+    {
+        public LightTextBox(){
+            this.Width = 100;
+            this.ForeColor = Color.LightBlue;
+            
+        }
+
+
+
+        public override Components.IComponent clone(ComponentInfo info)
+        {
+            var newControl = new LightTextBox();
+
+            //set info for newControl
+            //....
+
+            return newControl;
+
+        }
+
+        protected override void OnPaint(System.Windows.Forms.PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            System.Windows.Forms.TextRenderer.DrawText(e.Graphics, this.Text, this.Font, new Point(18, 7), Color.White);
+
+        }
+
+        protected override void OnPaintBackground(System.Windows.Forms.PaintEventArgs e)
+        {
+            base.OnPaintBackground(e);
+            this.BackColor = Color.White;
+            e.Graphics.FillRectangle(Brushes.LightBlue, 0, 0, this.Width, this.Height);
+        }
+  
+  
+    }
+
+       
+}
